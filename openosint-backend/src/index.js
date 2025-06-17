18c112620fff80a4153dcad3b0959daf7d9348f4
Artturi Kerminen <kerminenartturi@gmail.com>
2025-06-17 16:37:21 +0300

    add nodemon, express json

--- openosint-backend/src/index.js
+++ openosint-backend/src/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+
+
 const app = express()
 const port = 3000
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,4 +13,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
