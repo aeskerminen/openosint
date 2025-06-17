8e7ea124e24025e57577730d99db6b8c0d2f58af
Artturi Kerminen <kerminenartturi@gmail.com>
2025-06-17 17:55:28 +0300

    Add axios for request hnadling

--- openosint-frontend/src/features/datapoint_explorer/DatapointExplorer.tsx
+++ openosint-frontend/src/features/datapoint_explorer/DatapointExplorer.tsx
@@ -1,7 +1,14 @@
 const DatapointExplorer = () => {
-    return(
-        <div className="flex flex-col items-center justify-center flex-1">
+    return (
+        <div className="flex flex-col h-screen items-center justify-center flex-1">
             <p className="text-2xl font-bold mb-4">Datapoint Explorer</p>
+
+            {/* Form for uploading a picture (datapoint)*/}
+            <div className="flex-1">Upload</div>
+
+            {/* List of all datapoints fetched from the backend */}
+            <div className="flex-1">Datapoints</div>
+
         </div>
     )
 }
