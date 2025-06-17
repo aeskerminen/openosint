1c3c606c2aa73579d0d4a461d06ad1d8e48b8789
Artturi Kerminen <kerminenartturi@gmail.com>
2025-06-17 17:01:06 +0300

    boilerplate

--- openosint-backend/src/routes/datapoints.js
+++ openosint-backend/src/routes/datapoints.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
+router.post('/', (req, res) => {
+    const datapoint = req.body
+    res.status(201).send(`Datapoint created with ID: ${datapoint.id}`)
+})
+
 router.get('/', (req, res) => {
-  res.send('Datapoints root')
+    res.send('List of all datapoints')
+})
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+    res.send(`Datapoint with ID: ${id}`)
 })
 
 module.exports = router
