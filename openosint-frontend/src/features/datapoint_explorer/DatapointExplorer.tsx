478330f20b9769fc0eed522dfea61d132d621173
Artturi Kerminen <kerminenartturi@gmail.com>
2025-06-17 16:29:55 +0300

    prototyping...

--- openosint-frontend/src/features/datapoint_explorer/DatapointExplorer.tsx
+++ openosint-frontend/src/features/datapoint_explorer/DatapointExplorer.tsx
@@ -1,9 +1,9 @@
-const DataPointExplorer = () => {
+const DatapointExplorer = () => {
     return(
         <div className="flex flex-col items-center justify-center flex-1">
-            <p className="text-2xl font-bold mb-4">Data Point Explorer</p>
+            <p className="text-2xl font-bold mb-4">Datapoint Explorer</p>
         </div>
     )
 }
 
-export default DataPointExplorer;
+export default DatapointExplorer;
