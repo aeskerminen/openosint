830819a2492f5cb0838738c4b4b2a97644a35f7e
aeskerm <kerminenartturi@gmail.com>
2025-06-22 18:40:35 +0300

    prototype app structure

--- openosint-frontend/src/App.tsx
+++ openosint-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import DataPointExplorer from "./features/datapoint_explorer/DatapointExplorer";
 import DatapointViewer from "./features/datapoint_viewer/DatapointViewer";
 import type { Datapoint } from "./types/datapoint";
+import DatapointMap from "./features/datapoint_map/DatapointMap";
 
 const App = () => {
   const [selectedDatapoint, setSelectedDatapoint] = useState<Datapoint | null>(
@@ -16,6 +17,7 @@ const App = () => {
       ></DataPointExplorer>
       <div className="flex-1 h-full">
         <DatapointViewer datapointId={selectedDatapoint?._id} />
+        {false && <DatapointMap datapointId={selectedDatapoint?._id} />}
       </div>
     </div>
   );
--- openosint-frontend/src/features/datapoint_map/DatapointMap.tsx
+++ openosint-frontend/src/features/datapoint_map/DatapointMap.tsx
@@ -1,5 +1,21 @@
-const DatapointMap = () => {
-  return <div>DATAPOINT MAP</div>;
+import React from "react";
+import { useAppSelector } from "../../reduxHooks";
+import type { Datapoint } from "../../types/datapoint";
+
+interface DatapointMapProps {
+  datapointId: string | undefined;
+}
+
+const DatapointMap: React.FC<DatapointMapProps> = ({ datapointId }) => {
+  const datapoint = useAppSelector((state) =>
+    state.datapoints.value.find((dp: Datapoint) => dp._id === datapointId)
+  );
+
+  return (
+    <div className="flex-1 flex-col h-full bg-[#101010] p-4 rounded flex items-center justify-center text-gray-400">
+      <p>DATAPOINT MAP</p>
+    </div>
+  );
 };
 
 export default DatapointMap;
