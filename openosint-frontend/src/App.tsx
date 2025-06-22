0a0699fbfe3ee431278d5bde13f8122c8f6e4a53
aeskerm <kerminenartturi@gmail.com>
2025-06-22 18:14:07 +0300

    update app structure to be more standardized

--- openosint-frontend/src/App.tsx
+++ openosint-frontend/src/App.tsx
@@ -1,11 +1,24 @@
-import DataPointExplorer from "./features/datapoint_explorer/DatapointExplorer"
+import { useState } from "react";
+import DataPointExplorer from "./features/datapoint_explorer/DatapointExplorer";
+import DatapointViewer from "./features/datapoint_viewer/DatapointViewer";
+import type { Datapoint } from "./types/datapoint";
 
 const App = () => {
+  const [selectedDatapoint, setSelectedDatapoint] = useState<Datapoint | null>(
+    null
+  );
+
   return (
     <div className="flex flex-row items-center justify-center h-screen w-screen gap-2">
-      <DataPointExplorer></DataPointExplorer>
+      <DataPointExplorer
+        selectedDatapoint={selectedDatapoint}
+        setSelectedDatapoint={setSelectedDatapoint}
+      ></DataPointExplorer>
+      <div className="flex-1 h-full">
+        <DatapointViewer datapointId={selectedDatapoint?._id} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- openosint-frontend/src/features/datapoint_explorer/DatapointExplorer.tsx
+++ openosint-frontend/src/features/datapoint_explorer/DatapointExplorer.tsx
@@ -1,26 +1,23 @@
 import DatapointUploadContainer from "./components/DatapointUploadContainer";
 import DatapointListContainer from "./components/DatapointListContainer";
-import { useState } from "react";
 import type { Datapoint } from "../../types/datapoint";
-import DatapointViewer from "../datapoint_viewer/DatapointViewer";
 
-const DatapointExplorer = () => {
-  const [selectedDatapoint, setSelectedDatapoint] = useState<Datapoint | null>(
-    null
-  );
+interface DatapointExplorerProps {
+  selectedDatapoint: Datapoint | null;
+  setSelectedDatapoint: (datapoint: Datapoint) => void;
+}
 
+const DatapointExplorer: React.FC<DatapointExplorerProps> = ({
+  selectedDatapoint,
+  setSelectedDatapoint,
+}) => {
   return (
-    <div className="flex flex-row w-screen h-screen items-center justify-center flex-1">
-      <div className="flex flex-col gap-4 flex-1 max-w-5xl h-screen justify p-4">
-        <DatapointUploadContainer />
-        <DatapointListContainer
-          onSelect={setSelectedDatapoint}
-          selectedDatapoint={selectedDatapoint}
-        />
-      </div>
-      <div className="flex-1 h-full">
-        <DatapointViewer datapointId={selectedDatapoint?._id} />
-      </div>
+    <div className="flex flex-col gap-4 flex-1 max-w-5xl h-screen justify p-4">
+      <DatapointUploadContainer />
+      <DatapointListContainer
+        onSelect={setSelectedDatapoint}
+        selectedDatapoint={selectedDatapoint}
+      />
     </div>
   );
 };
