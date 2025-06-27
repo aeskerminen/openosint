1a89873717608e27311052836dddd2afd68fb641
aeskerm <kerminenartturi@gmail.com>
2025-06-27 10:22:23 +0300

    Add resizable ui

--- openosint-frontend/src/App.tsx
+++ openosint-frontend/src/App.tsx
@@ -5,21 +5,34 @@ import type { Datapoint } from "./types/datapoint";
 import DatapointMap from "./features/datapoint_map/DatapointMap";
 import Toolbar from "./features/toolbar/Toolbar";
 import type { ToolbarView } from "./types/toolbarView";
+import { ResizableBox } from "react-resizable";
+import "react-resizable/css/styles.css";
 
 const App = () => {
   const [selectedDatapoint, setSelectedDatapoint] = useState<Datapoint | null>(
     null
   );
 
-  const views : Array<ToolbarView> = [{ name: "DatapointViewer" }, { name: "DatapointMap" }];
+  const views: Array<ToolbarView> = [
+    { name: "DatapointViewer" },
+    { name: "DatapointMap" },
+  ];
   const [currentView, setCurrentView] = useState<string>(views[0].name);
 
   return (
     <div className="flex flex-row h-full w-full">
-      <DataPointExplorer
-        selectedDatapoint={selectedDatapoint}
-        setSelectedDatapoint={setSelectedDatapoint}
-      ></DataPointExplorer>
+      <ResizableBox
+        width={300}
+        height={Infinity}
+        minConstraints={[200, 100]}
+        maxConstraints={[500, Infinity]}
+        draggableOpts={{ handle: ".react-resizable-handle" }}
+      >
+        <DataPointExplorer
+          selectedDatapoint={selectedDatapoint}
+          setSelectedDatapoint={setSelectedDatapoint}
+        ></DataPointExplorer>
+      </ResizableBox>
       <div className="flex-1 h-full flex flex-col max-h-full max-w-full">
         <div
           id="problem"
