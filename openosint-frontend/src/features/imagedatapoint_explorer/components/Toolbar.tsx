88aab82d470b2e2c3067150b28ddd9f57f47ad35
aeskerm <kerminenartturi@gmail.com>
2025-06-28 18:27:04 +0300

    clean up toolbar.tsx

--- openosint-frontend/src/features/imagedatapoint_explorer/components/Toolbar.tsx
+++ openosint-frontend/src/features/imagedatapoint_explorer/components/Toolbar.tsx
@@ -8,15 +8,14 @@ interface ToolbarProps {
 
 const Toolbar: React.FC<ToolbarProps> = ({ views, setCurrentView }) => {
   return (
-    <nav className="w-full flex justify-center items-center p-2 bg-gradient-to-r from-[#232526] to-[#414345] mb-1">
+    <nav className="w-full flex justify-center items-center p-2 bg-black">
       <div className="flex flex-row gap-4">
         {views.map((view) => (
           <button
             key={view.name}
             data-testid={`toolbar-${view.name}-button`}
             onClick={() => setCurrentView(view.name)}
-            className="px-3 rounded-full font-semibold text-sm tracking-wide transition-all duration-200
-                                   bg-[#23272f] text-gray-200 shadow hover:bg-blue-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
+            className="p-3 rounded-full"
           >
             <span className="flex flex-row gap-4 items-center justify-center">
               {view.icon} {view.displayName}
